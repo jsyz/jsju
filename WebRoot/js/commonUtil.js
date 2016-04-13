b2e4aa1ea5471ead3641f60158e0d22018499e99
lq1997127 <477937204@qq.com>
2016-04-13 14:48:59 +0800

    冲突补提交
    冲突补提交

--- WebRoot/js/commonUtil.js
+++ WebRoot/js/commonUtil.js
@@ -53,7 +53,6 @@ function deleteAllCheckedUseros()
 					});
 		}
 }
-
 function deleteAllCheckedProjects()
 {
 		if(confirm('你确定删除这些项目吗？'))
@@ -85,6 +84,67 @@ function deleteAllCheckedProjects()
 					});
 		}
 }
-
-
-
+function deleteAllCheckedDevices()
+{
+		console.log("hello");
+		if(confirm('你确定删除这些设备吗？'))
+		{
+			var checkedIDs='';
+			for(var i =0;i<$(".indexID").length;i++)
+			{
+				if($(".indexID")[i].checked)
+				{
+					checkedIDs = checkedIDs+$(".indexID")[i].value+",";
+				}
+			}
+			console.log(checkedIDs);
+			$.ajax({   
+			            url:'deleteDevices',//这里是你的action或者servlert的路径地址   
+			            type:'post', //数据发送方式   
+			            async:false,
+			            data:{"checkedIDs":checkedIDs},
+			            dataType:'json',
+			            error: function(msg)
+			            { //失败   
+			            	console.log('删除失败.');   
+			            },   
+			            success: function(msg)
+			            { //成功
+			            	alert(msg.message);
+			            	location.replace(location.href);
+						}
+					});
+		}
+}
+function deleteAllCheckedPromans()
+{
+		console.log("hello");
+		if(confirm('你确定删除这些人员吗？'))
+		{
+			var checkedIDs='';
+			for(var i =0;i<$(".indexID").length;i++)
+			{
+				if($(".indexID")[i].checked)
+				{
+					checkedIDs = checkedIDs+$(".indexID")[i].value+",";
+				}
+			}
+			console.log(checkedIDs);
+			$.ajax({   
+			            url:'deletePromans',//这里是你的action或者servlert的路径地址   
+			            type:'post', //数据发送方式   
+			            async:false,
+			            data:{"checkedIDs":checkedIDs},
+			            dataType:'json',
+			            error: function(msg)
+			            { //失败   
+			            	console.log('删除失败.');   
+			            },   
+			            success: function(msg)
+			            { //成功
+			            	alert(msg.message);
+			            	location.replace(location.href);
+						}
+					});
+		}
+}
